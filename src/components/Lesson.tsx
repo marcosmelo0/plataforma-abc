bcf494c7d26a258ae0f01d7ac1689fc281cc6926
Marcos Melo <marcosmelo012@gmail.com>
2024-07-17 02:18:47 -0300

    feat: add card to the aside

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,48 @@
-export function Lesson() {
+import { CheckCircle, Lock } from "phosphor-react"
+import { isPast, format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonProps) {
+    const isLessonAvailabe = isPast(props.availableAt);
+    const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+        locale: ptBR
+    })
+
     return (
-        <h1>Lesson</h1>
+        <a href="">
+            <span className="text-gray-300">
+                {availableDateFormated}
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex items-center justify-between">
+                    {isLessonAvailabe ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20} className="text-green-300"/>
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                        <Lock size={20} className="text-orange-500" />
+                        Em breve
+                    </span>
+                    )}
+                    <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-blue-500 font-bold">
+                        {props.type === 'live' ? 'AO VIVO' : 'Aula Gravada'}
+                    </span>
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+            </div>
+        </a>
     )
 }
